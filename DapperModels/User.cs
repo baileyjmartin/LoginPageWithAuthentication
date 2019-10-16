f50de2ff2681089517b8785957b6f52dd8b7baf4
Bailey <bailey.martin.dev@gmail.com>
2019-10-16 12:18:50 -0600

    Initial Commit
    * Also adds knockout.js
    * Also adds Dapper and initial Dapper setup

---
+++ DapperModels/User.cs
@@ -0,0 +1,16 @@
+﻿namespace LoginPageWithAuthentication.DapperModels
+{
+    /// <summary>
+    /// Class utilized by Dapper to map to the dbo.AspNetUsers table
+    /// </summary>
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ DapperQueries/UserQueries.cs
@@ -0,0 +1,23 @@
+﻿using LoginPageWithAuthentication.DapperModels;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace LoginPageWithAuthentication.DapperQueries
+{
+    /// <summary>
+    /// Class containing queries to the dbo.AspNetUsers table.
+    /// Utilizes the <see cref="User"/> class
+    /// </summary>
+    public class UserQueries
+    {
+        public IEnumerable<User> GetUsers()
+        {
+            using (IDbConnection db = new SqlConnection())
+            {
+                return db.Query<User>("Select * From Author");
+            }
+        }
+    }
+}

20c29f807ecf157daacab30360a45dd552c4da00
Bailey <bailey.martin.dev@gmail.com>
2019-10-23 17:21:27 -0600

    Add Knockout Validation
    Add beginning stages of password validation utilizing Knockout.js

--- Models/ManageViewModels.cs
+++ Models/ManageViewModels.cs
@@ -28,7 +28,7 @@ namespace LoginPageWithAuthentication.Models
     public class SetPasswordViewModel
     {
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(12, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
@@ -47,7 +47,7 @@ namespace LoginPageWithAuthentication.Models
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(12, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 5)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
---
+++ Scripts/password-validator.js
@@ -0,0 +1,21 @@
+﻿var lettersDigitsRegEx = /([a-zA-Z])([0-9])/;
+var repeatCharactersRegEx = /(.{2,})\1/;
+
+$(function () {
+    ko.applyBindings(new PasswordViewModel());
+});
+
+function PasswordViewModel() {
+    var self = this;
+
+    self.password = ko.observable("");
+    self.correctLength = ko.computed(function() {
+        return (self.password().length > 4 && self.password().length < 13);
+    });
+    self.lettersDigits = ko.computed(function() {
+        return lettersDigitsRegEx.test(self.password());
+    });
+    self.repeatCharacters = ko.computed(function () {
+        return repeatCharactersRegEx.test(self.password());
+    });
+}
